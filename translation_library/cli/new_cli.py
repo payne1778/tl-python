2556762d6e7d71b5fc22b6d7553f73510431eea9
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-20 13:45:27 -0500

    add documentation and typing

--- translation_library/cli/new_cli.py
+++ translation_library/cli/new_cli.py
@@ -1,6 +1,7 @@
 from typing import Annotated, List  # pyright: ignore[reportDeprecated]
 
 import typer  # ignore-errors
+from typer.main import Typer
 
 from translation_library.utils.translation_utils import (
     get_i18n_obj,
@@ -9,43 +10,58 @@ from translation_library.utils.translation_utils import (
     is_supported,
 )
 
-cli = typer.Typer(no_args_is_help=True, suggest_commands=True)
+cli: Typer = typer.Typer(no_args_is_help=True, suggest_commands=True)
 
 
 @cli.command()
 def list(
     as_english: Annotated[bool, typer.Option("--english", "-e")] = False,
-    to_lowercase: Annotated[bool, typer.Option("--to-lowercase", "-t")] = False,
-):
+    use_casefold: Annotated[bool, typer.Option("--casefold", "-c")] = False,
+) -> None:
     """
     List supported languages in their native or english spelling.
 
     Args:
-        language (str): must have
-        as_english (Optional[bool]): not required
+        as_english (Optional[bool]): use english spelling when listing languages
+        use_casefold (Optional[bool]): use ASCII characters when listing languages
+
+    Example:
+    ```bash
+    $ python -m translation_library list -l en
+    $ python -m translation_library list -l de -a
+    $ python -m translation_library list -l jp -c
+    ```
     """
     if as_english:
-        print(get_languages_as_english_names(lowercase=to_lowercase))
-    print(get_languages(lowercase=to_lowercase))
+        print(get_languages_as_english_names(casefold=use_casefold))
+    print(get_languages(casefold=use_casefold))
 
 
 @cli.command()
 def translate(
-    language: Annotated[str, typer.Option("--language", "-l")],
+    language_code: Annotated[str, typer.Option("--language", "-l")],
     key_path: Annotated[str, typer.Option("--key-path", "-k")],
     args: Annotated[List[str], typer.Argument()] = [],
-):
+) -> None:
     """
     Translate any value from a language TOML file with a specified key.
 
     Args:
-        language (Annotated[str, typer.Option): _description_
-        key_path (Annotated[str, typer.Option): _description_
-        args (Annotated[dict, typer.Option): _description_
-    """
-    raw_i18n_str: str = str(get_i18n_obj(language.lower(), key_path))
+        language_code (str): language code of the TOML file to retrieve from
+        key_path (str): path to the i18n string to get in the language TOML file
+        args (Optional[dict[str, object]]): optional dictionary entries to format
+            into the i18n string's placeholder variables separated by spaces
 
-    # Example: "name=Blake", adds {"name": "Blake"} to the dictionary
+    Example:
+    ```bash
+    $ python -m translation_library translate -l en -k confirm
+    $ python -m translation_library translate -l de -k hello name=Blake
+    $ python -m translation_library translate -l jp -k notifications.new_message count=1
+    ```
+    """
+    raw_i18n_str: str = str(get_i18n_obj(language_code.lower(), key_path))
+
+    # If "name=Blake", adds {"name": "Blake"} to placeholder_args dictionary
     if args:
         placeholder_args: dict[str, str] = {
             k: v for k, v in (arg.split("=") for arg in args)
@@ -57,27 +73,39 @@ def translate(
 
 @cli.command()
 def supported(
-    language: Annotated[str, typer.Option("--language", "-l")],
-):
+    language_code: Annotated[str, typer.Option("--language", "-l")],
+) -> None:
     """
     Check if a given language is supported by your translation library.
 
     Args:
-        language (Annotated[str, typer.Option): _description_
+        language (str): the language code to check (ex: en, de, jp, etc.)
+
+    ```bash
+    $ python -m translation_library supported -l en
+    $ python -m translation_library supported -l de
+    $ python -m translation_library supported -l jp
+    ```
     """
-    print(is_supported(language.lower()))
+    print(is_supported(language_code.lower()))
 
 
 @cli.command()
 def i18n_print(
-    language: Annotated[str, typer.Option("--language", "-l")],
+    language_code: Annotated[str, typer.Option("--language", "-l")],
     key_path: Annotated[str, typer.Option("--key-path", "-k")],
-):
+) -> None:
     """
     Get any value from a language TOML file with a specified key.
 
     Args:
-        language (Annotated[str, typer.Option): _description_
-        key_path (Annotated[str, typer.Option): _description_
+        language (str): language of the i18n string(s) to print
+        key_path (str): path to the i18n string(s) in the TOML file. supports globbing
+
+    Example:
+    ```bash
+    $ python -m translation_library i18n-print -l de -k start.welcome
+    $ python -m translation_library i18n-print -l de -k settings.*
+    ```
     """
-    print(get_i18n_obj(language.lower(), key_path))
+    print(get_i18n_obj(language_code.lower(), key_path))
