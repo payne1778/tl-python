196c3e1aec1c85eb526efe9bfd5967c818028a5d
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-20 18:06:41 -0500

    make important dirs

--- build.sh
+++ build.sh
@@ -3,9 +3,15 @@
 set -e 
 
 RESOURCES_DIR="resources"
+TEMP_DIR="temp_resources_dir"
 RESOURCES_REPO_NAME="Translation-Library-Docs"
 RESOURCES_REPO_URL="https://github.com/payne1778/$RESOURCES_REPO_NAME"
 COMMIT="main"
 
+mkdir -p "$RESOURCES_DIR"
+mkdir -p "logs" 
+
 # Put contents of the docs repo into a new folder named with $DOCS_REPO_NAME
-git subtree add --prefix="$RESOURCES_DIR/" "$RESOURCES_REPO_URL" "$COMMIT" --squash
+git subtree add --prefix="$TEMP_DIR/" "$RESOURCES_REPO_URL" "$COMMIT" --squash
+
+mv "$TEMP_DIR/*" "$RESOURCES_DIR/"
